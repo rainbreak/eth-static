539f6c4e30cc5e9930d974ed549da8114d0ff8c8
rainbeam <rainbeam@riseup.net>
2016-05-01 13:09:45 +0100

    set cmake_prefix_path

--- cmake_build.sh
+++ cmake_build.sh
@@ -6,21 +6,5 @@ cmake -DCMAKE_BUILD_TYPE=Release \
       -DSOLIDITY=0 \
       \
       -DSTATIC_LINKING=1 \
-      \
-      -DJSONCPP_LIBRARY=/src/built/lib/libjsoncpp.a \
-      -DJSONCPP_INCLUDE_DIR=/src/built/include/ \
-      \
-      -DCRYPTOPP_LIBRARY=/src/built/lib/libcryptlib.a \
-      -DCRYPTOPP_INCLUDE_DIR=/src/built/include/ \
-      \
-      -DLEVELDB_LIBRARY=/src/built/lib/libleveldb.a \
-      -DLEVELDB_INCLUDE_DIR=/src/built/include/ \
-      \
-      -DMINIUPNPC_LIBRARY=/src/built/lib/libminiupnpc.a \
-      -DMINIUPNPC_INCLUDE_DIR=/src/built/include/ \
-      \
-      -DJSON_RPC_CPP_CLIENT_LIBRARY=/src/built/lib/libjsonrpccpp-client.a \
-      -DJSON_RPC_CPP_COMMON_LIBRARY=/src/built/lib/libjsonrpccpp-common.a \
-      -DJSON_RPC_CPP_SERVER_LIBRARY=/src/built/lib/libjsonrpccpp-server.a \
-      -DJSON_RPC_CPP_INCLUDE_DIR=/src/built/include/ \
+      -DCMAKE_PREFIX_PATH=/src/built/ \
       ..
